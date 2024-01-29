1c5e9b15df5e772ad2e9746ba7caa3a050df2d39
MohmmadAqel <MohaamadAqel1999@gmail.com>
2024-01-29 17:34:14 +0300

    test

--- src/Almosafer_Project/MyTestCases.java
+++ src/Almosafer_Project/MyTestCases.java
@@ -1,5 +1,6 @@
 package Almosafer_Project;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 public class MyTestCases {
 	String url = "https://www.almosafer.com/en";
 	WebDriver driver = new ChromeDriver();
@@ -19,50 +21,63 @@ public class MyTestCases {
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div/div/button[1]")).click();
 	}
-	@Test()
+	@Test(enabled = false)
 	public void CheckTheLanguage() {
-		String Acteuallanguage =  driver.findElement(By.tagName("html")).getAttribute("lang");
+		String Acteuallanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
 		Assert.assertEquals(Acteuallanguage, "en");
 	}
-	@Test()
+	@Test(enabled = false)
 	public void CheckTheCaruncy() {
 		String ActeualCaruncy = driver.findElement(By.xpath("//button[normalize-space()='SAR']")).getText();
 		Assert.assertEquals(ActeualCaruncy, "SAR");
 	}
-	@Test()
+	@Test(enabled = false)
 	public void CheckTheNumberofPhone() {
 		WebElement NumberOfPhone = driver.findElement(By.cssSelector("a[class='sc-hUfwpO bWcsTG'] strong"));
 		String ActualNumberOfPhone = NumberOfPhone.getText();
 		String ExpectedNumberOfPhone = "+966554400000";
 		Assert.assertEquals(ActualNumberOfPhone, ExpectedNumberOfPhone);
-		}
-	@Test()
+	}
+	@Test(enabled = false)
 	public void CheckQitafLogo() {
 		WebElement Footer = driver.findElement(By.tagName("footer"));
 		boolean QitafLogoIsDespaly = Footer.findElement(By.xpath("//div[@class='sc-fihHvN eYrDjb']")).isDisplayed();
-        Assert.assertEquals(QitafLogoIsDespaly, true);	
+		Assert.assertEquals(QitafLogoIsDespaly, true);
 	}
-	@Test()
+	@Test(enabled = false)
 	public void CheckHotelTapIsNotSelected() {
-		WebElement HotelTap = driver.findElement (By.id("uncontrolled-tab-example-tab-hotels"));
+		WebElement HotelTap = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
 		String ActualValue = HotelTap.getAttribute("aria-selected");
 		Assert.assertEquals(ActualValue, "false");
 	}
-	@Test()
+	@Test(enabled = false)
 	public void ChangeTheLangugeOfTheWebsite() throws InterruptedException {
-		String [] MyWebsite= {"https://www.almosafer.com/en","https://www.almosafer.com/ar"};
+		String[] MyWebsite = { "https://www.almosafer.com/en", "https://www.almosafer.com/ar" };
 		Random rand = new Random();
 		int RandomNumber = rand.nextInt(MyWebsite.length);
 		driver.get(MyWebsite[RandomNumber]);
 		Thread.sleep(2000);
 		String MyWebsiteURL = driver.getCurrentUrl();
-		if(MyWebsiteURL.contains("ar")) {
-			String Acteuallanguage =  driver.findElement(By.tagName("html")).getAttribute("lang");
+		if (MyWebsiteURL.contains("ar")) {
+			String Acteuallanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
 			Assert.assertEquals(Acteuallanguage, "ar");
-		}else if (MyWebsiteURL.contains("en")) {
-				String Acteuallanguage =  driver.findElement(By.tagName("html")).getAttribute("lang");
-				Assert.assertEquals(Acteuallanguage, "en");
-				}
+		} else if (MyWebsiteURL.contains("en")) {
+			String Acteuallanguage = driver.findElement(By.tagName("html")).getAttribute("lang");
+			Assert.assertEquals(Acteuallanguage, "en");
+		}
+	}
+	@Test()
+	public void CheckTheDateOfTheWebsite() {
+		LocalDate ExpectedReturnDate = LocalDate.now().plusDays(2);
+		LocalDate ExpectedDepatureDate = LocalDate.now().plusDays(1);
+		int ExpectedDayOfTheReturn = ExpectedReturnDate.getDayOfMonth();
+		int ExpectedDayOfTheDepature = ExpectedDepatureDate.getDayOfMonth();
+		WebElement ActualReturnDateOnWebsite = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-OxbzP edzUwL'] span[class='sc-cPuPxo LiroG']"));
+		WebElement ActualDepatureDateOnWebsite = driver
+				.findElement(By.cssSelector("div[class='sc-iHhHRJ sc-kqlzXE blwiEW'] span[class='sc-cPuPxo LiroG']"));
+        Assert.assertEquals(Integer.parseInt(ActualDepatureDateOnWebsite.getText()), ExpectedDayOfTheDepature);
+        Assert.assertEquals(Integer.parseInt(ActualReturnDateOnWebsite.getText()), ExpectedDayOfTheReturn);
 	}
 	@AfterTest
 	public void MyAfterTest() {
